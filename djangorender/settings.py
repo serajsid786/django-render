74fd13176803608ad089097d964fe0b16dbb1c36
serajsid786 <serajsid786@gmail.com>
2023-11-27 05:18:34 +0530

    Add initial2

--- djangorender/settings.py
+++ djangorender/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,6 +80,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES["default"] = dj_database_url.parse("postgres://demo_render_user:NxbDWKdUOMGRCdaP2BamoCenfe3a1Zam@dpg-clhq3bubbf9s73b2lll0-a.oregon-postgres.render.com/demo_render")
 
 
 # Password validation
